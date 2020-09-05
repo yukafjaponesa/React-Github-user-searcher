90d8a572d009c736949b6f12d78312828f0e09a5
Yuka Fukuzawa <yfukuzawa318@gmail.com>
2020-09-05 12:38:00 +0200

    Updates

---
+++ precache-manifest.3421ffbb8efe8cb2f29153debcb139dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a597d22b41876a0d5bdb62da70d123d",
+    "url": "/Github-user-seacher/index.html"
+  },
+  {
+    "revision": "b3f043dc07917a11a720",
+    "url": "/Github-user-seacher/static/js/2.e9dc4753.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Github-user-seacher/static/js/2.e9dc4753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13d30f978303a9db6186",
+    "url": "/Github-user-seacher/static/js/main.2fdfeb09.chunk.js"
+  },
+  {
+    "revision": "02e307530aee299c6fcf",
+    "url": "/Github-user-seacher/static/js/runtime-main.8093e51d.js"
+  }
+]);
